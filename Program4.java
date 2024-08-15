240644b9ef42574f7ae00cdeed12fdee5ef7752a
SabhaHaq <s.h.haq826@gmail.com>
2024-08-15 09:08:00 -0700

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -1,10 +1,22 @@
 //Youre on your own!
 //Jk ask for help if you need it (but also look at previous programs!!)
-
-
-
-//Paste console output below:
+public class Program4 {
+     public static void main(String[] args) {
+        //variables 
+         int num1 = 475; 
+         int num2 = 821; 
+         int num3 = 369;
+         int num4 = 562; 
+        int sum = (num1+num2+num3+num4); 
+        double average = (sum)/4.0;
+        System.out.println("The sum of the four numbers is " + sum); 
+        System.out.println("The average of the four numbers is " + average);
+}
+}
+         
+    
+     
+     //Paste console output below:
 /*
-
-
-*/
+The sum of the four numbers is 2227
+The average of the four numbers is 556.75
